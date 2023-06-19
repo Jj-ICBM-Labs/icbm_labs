6c5f29e7fce11bfd2f88b3d174d4d0ceaf7dc972
Vendetta <ananddechannel@gmail.com>
2023-06-19 21:20:58 +0900

    changed theme > Using AI : https://aicolors.co

--- src/components/Research.tsx
+++ src/components/Research.tsx
@@ -9,5 +9,8 @@ export default function Research({ research, startDate, endDate }: researchTypes
         <p className={styles.title}>
             {research}
         </p>
+        <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 15L17.6603 0H0.339746L9 15Z" fill="var(--text-200)" />
+        </svg>
     </div>
 }
