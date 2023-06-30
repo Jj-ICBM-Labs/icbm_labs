8ea019ad70164ab02e71338175546729336e8911
Vendetta <ananddechannel@gmail.com>
2023-07-01 04:03:03 +0900

    Add version viewer in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
 import styles from 'modules/styles/components/Footer.module.css';
+import { useEffect, useRef } from 'react';
 
 export default function Footer() {
+    const version = useRef<HTMLParagraphElement>(null);
+    useEffect(() => {
+        if (version.current) {
+            fetch("/api/getVersion").then((res) => {
+                res.json().then((json: { version: string }) => {
+                    version.current!.innerHTML = "Version : " + json.version;
+                })
+            })
+        };
+    }, []);
+
     return (
         <div className={styles.footer}>
-            Copyright &copy; 2023. ICBM_LABS. All rights reserved.
+            <p>Copyright &copy; 2023. ICBM_LABS. All rights reserved.</p>
+            <p ref={version}>Version : </p>
         </div >
     )
 }
