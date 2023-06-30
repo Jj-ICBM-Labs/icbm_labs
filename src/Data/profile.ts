6e6014529a9afeaa191faaf0e035e2b2145e9e55
Vendetta <ananddechannel@gmail.com>
2023-06-30 23:26:07 +0900

    fix mistake

--- src/Data/profile.ts
+++ src/Data/profile.ts
@@ -1,7 +1,10 @@
-export type profileTypes = {
-    name: string;
-    number: number;
-    bio?: string;
-    github?: string;
-    blog?: string;
-}
+import { profileTypes } from "modules/Types/profiles";
+
+export const profile: profileTypes[] = [
+    {
+        name: "성민우",
+        number: 22,
+        bio: "Devist",
+        github: "https://github.com/CistusF",
+    }
+]
--- src/Types/profiles.ts
+++ src/Types/profiles.ts
@@ -1,10 +1,7 @@
-import { profileTypes } from "modules/Data/profile";
-
-export const profile: profileTypes[] = [
-    {
-        name: "성민우",
-        number: 22,
-        bio: "Devist",
-        github: "https://github.com/CistusF",
-    }
-]
+export type profileTypes = {
+    name: string;
+    number: number;
+    bio?: string;
+    github?: string;
+    blog?: string;
+}
