1723755b79e626c9f75553c5c981c4a11e52a8e6
Vendetta <ananddechannel@gmail.com>
2024-05-27 13:26:04 +0900

    Added link
    Add interview link

--- src/pages/componets/ApplyLabs.tsx
+++ src/pages/componets/ApplyLabs.tsx
@@ -29,7 +29,7 @@ export default function ApplyLabs() {
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
                             <a
-                                href="#"
+                                href="https://naver.me/GSH4z8ZK"
                                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                             >
                                 연구원 지원
